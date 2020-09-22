87b132315b192088dcec65ba309bc165f6ee412f
Chai Nunes <chaiyawahoo@gmail.com>
2020-09-22 10:43:12 -0700

    basic functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+
+billInput.addEventListener("input", calculateTip);
+tipInput.addEventListener("input", calculateTip);
+
+function calculateTip() {
+    const billValue = billInput.value;
+    const tipValue = tipInput.value;
+
+    const tipAmount = billValue * tipValue / 100;
+
+    display.innerText = tipAmount.toFixed(2);
+}
+
+calculateTip();
