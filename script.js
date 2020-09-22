8edcc8b0cd49f8ee3ec39a0784e2beac22f8e5d3
Chai Nunes <chaiyawahoo@gmail.com>
2020-09-22 11:04:29 -0700

    challenge 2

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const display = document.getElementById("display");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
+const peopleInput = document.getElementById("people");
 
 billInput.addEventListener("input", calculateTip);
 tipInput.addEventListener("input", calculateTip);
+peopleInput.addEventListener("input", calculateTip);
 
 function calculateTip() {
     const billValue = billInput.value;
     const tipValue = tipInput.value;
+    const peopleValue = peopleInput.value;
 
-    const tipAmount = billValue * tipValue / 100;
+    const tipAmount = billValue * tipValue / 100 / peopleValue;
 
     display.innerText = `Tip Amount: $${tipAmount.toFixed(2)}`;
 }
