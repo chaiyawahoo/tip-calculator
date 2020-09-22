2bdb27242ce5069836aeb74f579877a8431fc23f
Chai Nunes <chaiyawahoo@gmail.com>
2020-09-22 12:18:57 -0700

    update

--- script.js
+++ script.js
@@ -2,21 +2,55 @@ const display = document.getElementById("display");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
 const peopleInput = document.getElementById("people");
+const subtractButtons = Array.from(document.getElementsByClassName("subtract"));
+const addButtons = Array.from(document.getElementsByClassName("add"));
 
 billInput.addEventListener("input", calculateTip);
 tipInput.addEventListener("input", calculateTip);
 peopleInput.addEventListener("input", calculateTip);
 
+subtractButtons.forEach(applySubtractEventListener);
+addButtons.forEach(applyAddEventListener);
+
 function calculateTip() {
-    const billValue = parseInt(billInput.value);
-    const tipValue = parseInt(tipInput.value);
-    const peopleValue = parseInt(peopleInput.value);
+    let billValue = parseInt(billInput.value);
+    let tipValue = parseInt(tipInput.value);
+    let peopleValue = parseInt(peopleInput.value);
+
+    if (isNaN(billValue) || billValue < 0) billValue = 0;
+    if (isNaN(tipValue) || tipValue < 0) tipValue = 0;
+    if (isNaN(peopleValue) || peopleValue < 1) peopleValue = 1;
+    billInput.value = billValue;
+    tipInput.value = tipValue;
+    peopleInput.value = peopleValue;
 
     const tipAmount = billValue * tipValue / 100;
     const totalAmount = tipAmount + billValue;
 
     display.innerHTML = `<div>Tip Per Person: $${(tipAmount / peopleValue).toFixed(2)}</div>
     <div>Total Per Person: $${(totalAmount / peopleValue).toFixed(2)}</div>`;
+}
+
+function applySubtractEventListener(button) {
+    button.addEventListener("click", function (e) {
+        const inputElement = e.target.parentElement.children[1];
+        if (inputElement.name === "tip" && inputElement.value > 0) {
+            inputElement.value--;
+        } else if (inputElement.name === "people" && inputElement.value > 1) {
+            inputElement.value--;
+        }
+        calculateTip();
+    });
+}
+
+function applyAddEventListener(button) {
+    button.addEventListener("click", function (e) {
+        const inputElement = e.target.parentElement.children[1];
+        if (inputElement.name === "tip" || inputElement.name === "people") {
+            inputElement.value++;
+        }
+        calculateTip();
+    });
 }
 
 calculateTip();
